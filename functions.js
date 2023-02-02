7f7ec61f23cb1788172e3b84d3677b956d0f8b27
Kseniia <kvxoxlova2005@mail.ru>
2023-02-02 08:03:26 +0300

    Рабочий код

--- functions.js
+++ functions.js
@@ -1,5 +1,5 @@
-const getNumber = function(a = 101, b = 0){
-    return Math.floor(Math.random() * ((a - b) + b));
+const getNumber = function(a = 101, b = 0) {
+    return Math.floor(Math.random() * (a - b) + b);
 }
 const getColor = function(a = 100){
     return `rgba(${getNumber(256)}, ${getNumber(256)}, ${getNumber(256)}, ${a / 100})`;
